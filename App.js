013b84d09e6b42ebfed62dc0f91231a607fbac82
KORE NAGARAJU <nagaraj13562001@gmail.com>
2023-10-31 10:30:00 +0530

    updated project

--- App.js
+++ App.js
@@ -1,32 +1,15 @@
 const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
-<<<<<<< HEAD
 let searchValue = undefined;
 let SearchInput = document.querySelector('.main-input');
 let SearchButton = document.querySelector('.search-icon');
 SearchButton.addEventListener('click', () => {
     searchValue = SearchInput.value
-=======
-let card = `
-<div class="card" style="width: 18rem;">
-<div class="card-body">
-  <h5 class="card-title"></h5>
-  <p class="card-text"></p>
-  <a href="#" class="btn btn-primary">Delete</a>
-</div>
-</div>
-`
-let SearchInput = document.querySelector('.main-input');
-let SearchButton = document.querySelector('.search-icon');
-SearchButton.addEventListener('click', () => {
-    let searchValue = SearchInput.value
->>>>>>> 4962e4d68d786949d41b14d2cb2a363738069a10
     async function fetchData(url) {
         let fetchedData = await fetch(url + searchValue);
         let jsonValue = await fetchedData.json();
         let result = jsonValue[0].meanings[0].definitions[0].definition;
         const resultComponent = document.querySelector('.result-info');
         resultComponent.innerHTML = result;
-<<<<<<< HEAD
         //console.log(result);
         localStorageHistroy(result);
     }
@@ -62,9 +45,3 @@ document.querySelector('.nav-bar-history').addEventListener('click', () => {
         HistoryContainerCard.innerHTML += card;
     }
 })
-=======
-        console.log(result);
-    }
-    fetchData(url);
-});
->>>>>>> 4962e4d68d786949d41b14d2cb2a363738069a10
